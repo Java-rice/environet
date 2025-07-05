8d91546d0dcd7eb27a646608a9a37b689421bd3f
Java-rice <johnmark.p.peroche@gmail.com>
2025-07-05 23:16:25 +0800

    feat: style navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { Home } from "./pages/Home";
 import { Navbar } from "./components/Navbar";
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-[#FBE9E7] text-[#424242] transition-opacity duration-700 pt-20">
       <Navbar />
-      <div>
+      <div className="container mx-auto px-4 py-6">
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,29 +5,51 @@ import { Link } from "react-router";
 export const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   return (
-    <nav>
-      <div>
-        <div>
-          <Link to={"/"} className="font-bold">
+    <nav className="fixed top-0 w-full z-40 shadow-sm backdrop-blur-lg">
+      <div className="max-w-5xl mx-auto py-4">
+        <div className="flex justify-between items-center h-16 mx-8">
+          <Link to={"/"} className="font-bold text-xl">
             <span className="text-[#4CAF50]">Enviro</span>net
           </Link>
-          <div>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/create"}>Post</Link>
-            <Link to={"/community"}>Community</Link>
-            <Link to={"/community/create"}>Create Community</Link>
-            <Link to={"/event"}>Event</Link>
-            <Link to={"/donate"}>Donate</Link>
-            <button>
-              <CircleUser />
-            </button>
+          <div className="hidden md:flex items-center space-x-6">
+            <Link to={"/"} className="hover:text-[#4CAF50] transition-colors">
+              Home
+            </Link>
+            <Link
+              to={"/create"}
+              className="hover:text-[#4CAF50] transition-colors"
+            >
+              Post
+            </Link>
+            <Link
+              to={"/community"}
+              className="hover:text-[#4CAF50] transition-colors"
+            >
+              {" "}
+              Community
+            </Link>
+            <Link
+              to={"/event"}
+              className="hover:text-[#4CAF50] transition-colors"
+            >
+              Event
+            </Link>
+            <Link
+              to={"/donate"}
+              className="hover:text-[#4CAF50] transition-colors"
+            >
+              Donate
+            </Link>
           </div>
 
           {/* Hamburger */}
-          <div>
+          <div className="flex gap-4">
+            <button>
+              <CircleUser />
+            </button>
             <button
               onClick={() => setMenuOpen((prev) => !prev)}
-              className="cursor-pointer"
+              className="cursor-pointer md:hidden"
             >
               {isMenuOpen ? <X /> : <Menu />}
             </button>
@@ -35,14 +57,38 @@ export const Navbar = () => {
 
           {/* Mobile Menu */}
           {isMenuOpen && (
-            <div className="">
-              <div>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/create"}>Post</Link>
-                <Link to={"/community"}>Community</Link>
-                <Link to={"/community/create"}>Create Community</Link>
-                <Link to={"/event"}>Event</Link>
-                <Link to={"/donate"}>Donate</Link>
+            <div className=" absolute md:hidden top-20 right-0 left-0">
+              <div className="flex flex-col  justify-center bg-[#FBE9E7] px-2 pt-2 pb-3 space-y-1">
+                <Link
+                  to={"/"}
+                  className="block px-3 text-center shadow-md py-2 rounded-md text-base font-medium hover:text-[#4CAF50] "
+                >
+                  Home
+                </Link>
+                <Link
+                  to={"/create"}
+                  className="block px-3 py-2 text-center shadow-md rounded-md text-base font-medium hover:text-[#4CAF50]"
+                >
+                  Post
+                </Link>
+                <Link
+                  to={"/community"}
+                  className="block px-3 py-2 text-center shadow-md rounded-md text-base font-medium hover:text-[#4CAF50]"
+                >
+                  Community
+                </Link>
+                <Link
+                  to={"/event"}
+                  className="block px-3 py-2 text-center shadow-md rounded-md text-base font-medium hover:text-[#4CAF50]"
+                >
+                  Event
+                </Link>
+                <Link
+                  to={"/donate"}
+                  className="block px-3 py-2 text-center shadow-md rounded-md text-base font-medium hover:text-[#4CAF50]"
+                >
+                  Donate
+                </Link>
               </div>
             </div>
           )}
